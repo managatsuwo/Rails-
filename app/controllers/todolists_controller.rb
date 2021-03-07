a26ba6e540c68e5b1621a04ca99fccd9d70c0a14
EC2 Default User <ec2-user@ip-172-31-89-50.ec2.internal>
2021-03-07 12:42:07 +0000

    Rails7 Finish

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -5,11 +5,11 @@ class TodolistsController < ApplicationController
   end
 def create
   #1　データを新規登録するためのインスタンス作成
-  list = List.new(list_params)
+   list = List.new(list_params)
   #2　データをデータベースに保存するためのsaveメソッド実行
-  list.save
+   list.save
   #3　トップ画面へりだいれくと
-  redirect_to todolist_path(list.id)
+   redirect_to todolist_path(list.id)
 end
 def index
   @todolists=List.all
@@ -29,6 +29,6 @@ end
 private
 #ストロングパラメータ
 def list_params
-  params.require(:list).permit(:title,:body)
+ params.require(:list).permit(:title, :body, :image)
 end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile .secret_key = 'e7c919fec0d324f676fd0aebead6c995e13506aa49c7fa80db6d60ae1714b58ac3f13c401621129cfb6a27d502fc1a887c5f3ca562a7bfe0077b6cb813b88a02'
